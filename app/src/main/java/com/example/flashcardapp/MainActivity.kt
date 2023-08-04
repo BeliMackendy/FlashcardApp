4e3f9323393c972a09b68b3b7bea19d78dca4958
BeliMackendy <belizaire.mackendy@gmail.com>
2023-08-04 12:48:29 -0400

    Build in logic to show the answer side when the card is tapped

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.flashcardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.flashcard_question).setOnClickListener(View.OnClickListener {
+            findViewById<TextView>(R.id.flashcard_question).visibility = View.INVISIBLE
+            findViewById<TextView>(R.id.flashcard_answer).visibility = View.VISIBLE
+        })
     }
 }
