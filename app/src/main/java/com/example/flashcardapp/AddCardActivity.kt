92f43ae4e9d4f2432f8ab221197f10470fb46b66
BeliMackendy <belizaire.mackendy@gmail.com>
2023-08-11 12:35:48 -0400

    Add a Cancel button to the 'Add Card Screen'

--- app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
@@ -1,11 +1,20 @@
 package com.example.flashcardapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class AddCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
+
+        val btCancel = findViewById<ImageView>(R.id.bt_cancel)
+
+        btCancel.setOnClickListener(View.OnClickListener {
+            finish()
+        })
     }
 }
