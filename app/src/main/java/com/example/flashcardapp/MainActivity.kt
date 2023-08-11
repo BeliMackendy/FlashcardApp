43455791aa6d6995c4bbb5331f6f4164f555ef3b
BeliMackendy <belizaire.mackendy@gmail.com>
2023-08-11 11:40:16 -0400

    Add a ‘+’ button that takes the user to new ‘Add Card Screen’

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.flashcardapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -17,13 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         val flashcard_question = findViewById<TextView>(R.id.flashcard_question)
         val flashcard_answer = findViewById<TextView>(R.id.flashcard_answer)
-        val txt_A = findViewById<TextView>(R.id.txt_A)
-        val txt_B = findViewById<TextView>(R.id.txt_B)
-        val txt_C = findViewById<TextView>(R.id.txt_C)
-        val rl_choice = findViewById<RelativeLayout>(R.id.rl_choice)
-        val toggle_choices_visibility = findViewById<ImageView>(R.id.toggle_choices_visibility)
-
-        var isShowingAnswers = true
+        val btAdd = findViewById<ImageView>(R.id.bt_add)
 
         flashcard_question.setOnClickListener(View.OnClickListener {
             flashcard_question.visibility = View.INVISIBLE
@@ -35,68 +31,9 @@ class MainActivity : AppCompatActivity() {
             flashcard_answer.visibility = View.INVISIBLE
         })
 
-
-        txt_A.setOnClickListener(View.OnClickListener {
-            if(txt_A.text==flashcard_answer.text)
-            {
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_answer, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-            }
-            else{
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_error, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_answer, null))
-            }
-        })
-
-        txt_B.setOnClickListener(View.OnClickListener {
-            if(txt_B.text==flashcard_answer.text)
-            {
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_answer, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-            }
-            else{
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_error, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_answer, null))
-            }
-        })
-
-        txt_C.setOnClickListener(View.OnClickListener {
-            if(txt_C.text==flashcard_answer.text)
-            {
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_answer, null))
-            }
-            else{
-                txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-                txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_error, null))
-            }
-        })
-
-        rl_choice.setOnClickListener(View.OnClickListener {
-            txt_A.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-            txt_B.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-            txt_C.setBackground(getResources().getDrawable(R.drawable.card_background_choice, null))
-        })
-
-        toggle_choices_visibility.setOnClickListener(View.OnClickListener {
-            if(isShowingAnswers==true)
-            {
-                isShowingAnswers = false
-                toggle_choices_visibility.setImageResource(R.drawable.eye_off_lined)
-                rl_choice.visibility = View.INVISIBLE
-            }
-            else
-            {
-                isShowingAnswers = true
-                toggle_choices_visibility.setImageResource(R.drawable.eye_lined)
-                rl_choice.visibility = View.VISIBLE
-            }
+        btAdd.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,AddCardActivity::class.java)
+            startActivity(intent)
         })
     }
 }
